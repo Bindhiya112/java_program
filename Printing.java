7752dd11ab6db254028b8905ec96fbc636ee1635
Bindhiya T <bindhiyat9889@gmail.com>
2026-08-06 16:31:21 +0530

    Add files via upload

---
+++ Printing.java
@@ -0,0 +1,11 @@
+public class Printing {
+
+    public static void main(String[]args){
+        String name ="Rajesh";
+        int age = 20;
+        double salary =97500.00;
+        System.out.printf("Name: %s, age: %d%n" ,name,age);
+        System.out.printf("Salary: $%,.2f", salary);
+    }
+    
+}
