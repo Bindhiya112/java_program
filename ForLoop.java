f36ce2dca428bbe3cabf021ebb53c9bd9924504b
Bindhiya T <bindhiyat9889@gmail.com>
2026-08-09 12:32:25 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        int a = 5;
+        for(int i = 1;i<= a;i++){
+            System.out.println("Good Morning");
+        }
+    }
+    
+}
