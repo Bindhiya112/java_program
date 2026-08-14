b834e2cba1aabd49b94ea14d9add3201c6c1793b
Bindhiya T <bindhiyat9889@gmail.com>
2026-08-14 11:40:57 +0530

    Add files via upload

---
+++ PrimeCheck.java
@@ -0,0 +1,17 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 1;
+        boolean isPrime = true;
+        for (int i =2; i<= num-1; i++){
+            if(num%i==0){
+                isPrime=false;
+            }
+        }
+        if(isPrime==true){
+            System.out.println("prime");
+        }else{
+            System.out.println("not prime");
+        }
+    }
+    
+}
